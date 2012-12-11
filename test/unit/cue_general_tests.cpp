23425a5494a2fd831b8bde1c27f49579a29906c5
Dale Karp <me@dale.io>
2012-12-11 00:38:20 -0500

    adding skeleton for file

--- test/unit/cue_general_tests.cpp
+++ test/unit/cue_general_tests.cpp
@@ -1,4 +1,197 @@
 #include "cue_testfixture"
 class CueGeneralTests : public CueTest { };
 
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
 
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
+
+/*
+ * This tests the bare minimum required for a valid WebVTT file with
+ * one cue and no content.
+ * ACCORDING TO WEBVTT SPEC DATE:
+ * WHICH STATED:
+ *
+*/
+TEST_F(CueGeneralTests, CueBareMinimum)
+{
+  loadVtt();
+
+}
