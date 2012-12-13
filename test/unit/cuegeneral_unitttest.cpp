e8a8bc6506aabd41cae3390ecd1a4216f3f3769e
Dale Karp <me@dale.io>
2012-12-12 21:14:13 -0500

    all commented!

--- test/unit/cuegeneral_unitttest.cpp
+++ test/unit/cuegeneral_unitttest.cpp
@@ -4,9 +4,24 @@ class CueGeneralTests : public CueTest { };
 /*
  * This tests the bare minimum required for a valid WebVTT file with
  * one cue and no content.
- * ACCORDING TO WEBVTT SPEC DATE:
+ * CURRENTY DISABLED: Parser does not recognized
+ * a cue with no payload as a cue, is returning 0
+ * cues founf.
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue
  * WHICH STATED:
- *
+ * A WebVTT cue consists of the following components, in the given order:
+ * Optionally, a WebVTT cue identifier followed by a WebVTT line terminator.
+ * WebVTT cue timings.
+ * Optionally, one or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION 
+ * (tab) characters followed by WebVTT cue settings.
+ * A WebVTT line terminator.
+ * The cue payload: either WebVTT cue text, WebVTT chapter title text, or WebVTT 
+ * metadata text, but it must not contain the substring "-->" (U+002D HYPHEN-MINUS, 
+ * U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN).
+ * 
+ * Each payload consists of "zero or more" objects, making
+ * them optional.
 */
 TEST_F(CueGeneralTests, DISABLED_BareMinimum)
 {
@@ -16,9 +31,18 @@ TEST_F(CueGeneralTests, DISABLED_BareMinimum)
 /*
  * This tests the bare minimum required for a valid WebVTT file with
  * one cue and sample content.
- * ACCORDING TO WEBVTT SPEC DATE:
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue
  * WHICH STATED:
- *
+ * A WebVTT cue consists of the following components, in the given order:
+ * Optionally, a WebVTT cue identifier followed by a WebVTT line terminator.
+ * WebVTT cue timings.
+ * Optionally, one or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION 
+ * (tab) characters followed by WebVTT cue settings.
+ * A WebVTT line terminator.
+ * The cue payload: either WebVTT cue text, WebVTT chapter title text, or WebVTT 
+ * metadata text, but it must not contain the substring "-->" (U+002D HYPHEN-MINUS, 
+ * U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN).
 */
 TEST_F(CueGeneralTests, BareMinimumWithContent)
 {
@@ -29,9 +53,22 @@ TEST_F(CueGeneralTests, BareMinimumWithContent)
  * This tests the highest legal values in a timestamp.
  * This does not included hours, as the highest legal value
  * of an hour would be the highest value of an int (system-dependent)
- * ACCORDING TO WEBVTT SPEC DATE:
+ * CURRENTY DISABLED: Parser does not recognized
+ * a cue with no payload as a cue, is returning 0
+ * cues founf.
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-timestamp
  * WHICH STATED:
- *
+ * A WebVTT timestamp representing hours hours, minutes minutes, seconds seconds, and thousandths of a 
+ * second seconds-frac, consists of the following components, in the given order:
+ * Optionally (required if hour is non-zero):
+ * Two or more ASCII digits, representing the hours as a base ten integer.
+ * A U+003A COLON character (:)
+ * Two ASCII digits, representing the minutes as a base ten integer in the range 0 ≤ minutes ≤ 59.
+ * A U+003A COLON character (:)
+ * Two ASCII digits, representing the seconds as a base ten integer in the range 0 ≤ seconds ≤ 59.
+ * A U+002E FULL STOP character (.).
+ * Three ASCII digits, representing the thousandths of a second seconds-frac as a base ten integer.
 */
 TEST_F(CueGeneralTests, DISABLED_HighestValues)
 {
@@ -41,9 +78,20 @@ TEST_F(CueGeneralTests, DISABLED_HighestValues)
 /*
  * This tests a bunch of space and tab characters in between
  * timestamp tokens
- * ACCORDING TO WEBVTT SPEC DATE:
+ * CURRENTY DISABLED: Parser does not recognized
+ * a cue with no payload as a cue, is returning 0
+ * cues founf.
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue-timings
  * WHICH STATED:
- *
+ * The WebVTT cue timings part of a WebVTT cue consists of the following components, in the given order:
+ * A WebVTT timestamp representing the start time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than or equal to the start time offsets of all previous cues in the file.
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * The string "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN).
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * A WebVTT timestamp representing the end time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than the start time offset of the cue.
 */
 TEST_F(CueGeneralTests, DISABLED_SpaceTabs)
 {
@@ -53,9 +101,17 @@ TEST_F(CueGeneralTests, DISABLED_SpaceTabs)
 /*
  * This tests using tabs instead of sapces
  * in between timesetamp tokens
- * ACCORDING TO WEBVTT SPEC DATE:
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue-timings
  * WHICH STATED:
- *
+ * The WebVTT cue timings part of a WebVTT cue consists of the following components, in the given order:
+ * A WebVTT timestamp representing the start time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than or equal to the start time offsets of all previous cues in the file.
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * The string "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN).
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * A WebVTT timestamp representing the end time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than the start time offset of the cue.
 */
 TEST_F(CueGeneralTests, Tabs)
 {
@@ -64,9 +120,18 @@ TEST_F(CueGeneralTests, Tabs)
 
 /*
  * This tests the optional hours token in the timestamp
- * ACCORDING TO WEBVTT SPEC DATE:
+ * CURRENTY DISABLED: Parser does not recognized
+ * a cue with no payload as a cue, is returning 0
+ * cues founf.
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-timestamp
  * WHICH STATED:
- *
+ * A WebVTT timestamp representing hours hours, minutes minutes, seconds seconds, 
+ * and thousandths of a second seconds-frac, consists of the following components, in the given order:
+ * Optionally (required if hour is non-zero):
+ * Two or more ASCII digits, representing the hours as a base ten integer.
+ * A U+003A COLON character (:)
+ *.[...]
 */
 TEST_F(CueGeneralTests, DISABLED_Hours)
 {
@@ -75,9 +140,13 @@ TEST_F(CueGeneralTests, DISABLED_Hours)
 
 /*
  * This tests multiple cues in a vtt file
- * ACCORDING TO WEBVTT SPEC DATE:
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue-timings
  * WHICH STATED:
- *
+ * The WebVTT cue timings part of a WebVTT cue consists of the following components, in the given order:
+ * A WebVTT timestamp representing the start time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than or equal to the start time offsets of all previous cues in the file.
+ * [...]
 */
 TEST_F(CueGeneralTests, MultiCues)
 {
@@ -86,9 +155,11 @@ TEST_F(CueGeneralTests, MultiCues)
 
 /*
  * This tests nested cues.
- * ACCORDING TO WEBVTT SPEC DATE:
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-file-using-only-nested-cues
  * WHICH STATED:
- *
+ * A WebVTT file whose cues all have an end time offset x greater than or equal to the end time offsets 
+ * of all the cues whose start time offsets are less than x is said to be a WebVTT file using only nested cues.
 */
 TEST_F(CueGeneralTests, NestedCues)
 {
@@ -97,8 +168,19 @@ TEST_F(CueGeneralTests, NestedCues)
 
 /*
  * This tests spaces missing between time stamp tokens
- * ACCORDING TO WEBVTT SPEC DATE:
+ * Because the parser does not validate, the parser should
+ * attempt to parse this. Parse.
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue-timings
  * WHICH STATED:
+ * The WebVTT cue timings part of a WebVTT cue consists of the following components, in the given order:
+ * A WebVTT timestamp representing the start time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than or equal to the start time offsets of all previous cues in the file.
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * The string "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN).
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * A WebVTT timestamp representing the end time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than the start time offset of the cue.
  *
 */
 TEST_F(CueGeneralTests, MissingSpaces)
@@ -109,8 +191,19 @@ TEST_F(CueGeneralTests, MissingSpaces)
 /*
  * This tests a missing space between left timestamp and arrow token
  * one cue and no content.
- * ACCORDING TO WEBVTT SPEC DATE:
+ * Because the parser does not validate, the parser should
+ * attempt to parse this. Parse.
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue-timings
  * WHICH STATED:
+ * The WebVTT cue timings part of a WebVTT cue consists of the following components, in the given order:
+ * A WebVTT timestamp representing the start time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than or equal to the start time offsets of all previous cues in the file.
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * The string "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN).
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * A WebVTT timestamp representing the end time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than the start time offset of the cue.
  *
 */
 TEST_F(CueGeneralTests, MissingSpaceLeft)
@@ -121,8 +214,19 @@ TEST_F(CueGeneralTests, MissingSpaceLeft)
 /*
  * This tests a missing space between right timestamp and arrow token
  * one cue and no content.
- * ACCORDING TO WEBVTT SPEC DATE:
+ * Because the parser does not validate, the parser should
+ * attempt to parse this. Parse.
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue-timings
  * WHICH STATED:
+ * The WebVTT cue timings part of a WebVTT cue consists of the following components, in the given order:
+ * A WebVTT timestamp representing the start time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than or equal to the start time offsets of all previous cues in the file.
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * The string "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN).
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * A WebVTT timestamp representing the end time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than the start time offset of the cue.
  *
 */
 TEST_F(CueGeneralTests, MissingSpaceRight)
@@ -134,8 +238,22 @@ TEST_F(CueGeneralTests, MissingSpaceRight)
  * This tests the effect of non-numeric characters
  * in the timestamp.
  * one cue and no content.
- * ACCORDING TO WEBVTT SPEC DATE:
+ * Because the parser does not validate, the parser should
+ * attempt to parse this. Parse.
+ * CURRENTY DISABLED: Parser segmentation faults
+ * when trying to parse timestamps and encountering
+ * any character it is not expecting.
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue-timings
  * WHICH STATED:
+ * The WebVTT cue timings part of a WebVTT cue consists of the following components, in the given order:
+ * A WebVTT timestamp representing the start time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than or equal to the start time offsets of all previous cues in the file.
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * The string "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN).
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * A WebVTT timestamp representing the end time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than the start time offset of the cue.
  *
 */
 TEST_F(CueGeneralTests, DISABLED_NoNumsInTimeStamp)
@@ -147,9 +265,18 @@ TEST_F(CueGeneralTests, DISABLED_NoNumsInTimeStamp)
  * This tests demonstrates that a WebVTT file
  * fails validation when the --> token is anything
  * but three characters in a row.
- * ACCORDING TO WEBVTT SPEC DATE:
+ * Because the parser does not validate, the parser should
+ * attempt to parse this. Parse.
+ * CURRENTY DISABLED: Parser does not recognized
+ * a cue with no payload as a cue, is returning 0
+ * cues founf.
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue-timings
  * WHICH STATED:
- *
+ * The WebVTT cue timings part of a WebVTT cue consists of the following components, in the given order:
+ * [...]
+ * The string "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN).
+ * [...]
 */
 TEST_F(CueGeneralTests, DISABLED_ArrowsWrongDirection)
 {
@@ -158,9 +285,22 @@ TEST_F(CueGeneralTests, DISABLED_ArrowsWrongDirection)
 
 /*
  * This tests demonstrates bad timestamps.
- * one cue and no content.
- * ACCORDING TO WEBVTT SPEC DATE:
+ * Because the parser does not validate, the parser should
+ * attempt to parse this. Parse.
+ * CURRENTY DISABLED: Parser segmentation faults
+ * when trying to parse timestamps and encountering
+ * any character it is not expecting.
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue-timings
  * WHICH STATED:
+ * The WebVTT cue timings part of a WebVTT cue consists of the following components, in the given order:
+ * A WebVTT timestamp representing the start time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than or equal to the start time offsets of all previous cues in the file.
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * The string "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN).
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * A WebVTT timestamp representing the end time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than the start time offset of the cue.
  *
 */
 TEST_F(CueGeneralTests, DISABLED_BadTimeStamps1)
@@ -170,8 +310,20 @@ TEST_F(CueGeneralTests, DISABLED_BadTimeStamps1)
 
 /*
  * This tests demonstrates bad timestamps.
- * ACCORDING TO WEBVTT SPEC DATE:
+ * CURRENTY DISABLED: Parser segmentation faults
+ * when trying to parse timestamps and encountering
+ * any character it is not expecting.
+ * ACCORDING TO WEBVTT SPEC DATE: 2012-12-12
+ * http://dev.w3.org/html5/webvtt/#webvtt-cue-timings
  * WHICH STATED:
+ * The WebVTT cue timings part of a WebVTT cue consists of the following components, in the given order:
+ * A WebVTT timestamp representing the start time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than or equal to the start time offsets of all previous cues in the file.
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * The string "-->" (U+002D HYPHEN-MINUS, U+002D HYPHEN-MINUS, U+003E GREATER-THAN SIGN).
+ * One or more U+0020 SPACE characters or U+0009 CHARACTER TABULATION (tab) characters.
+ * A WebVTT timestamp representing the end time offset of the cue. The time represented by this WebVTT 
+ * timestamp must be greater than the start time offset of the cue.
  *
 */
 TEST_F(CueGeneralTests, DISABLED_BadTimeStamps2)
